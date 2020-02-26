7b7fe37433bdabff08ad52fa303b2710a4921d0e
Longlong <lxu3@unh.newhaven.edu>
2020-02-25 22:28:59 -0500

    Initial commit

---
+++ app/src/main/java/com/prehax/gomovie/MainActivity.java
@@ -0,0 +1,36 @@
+package com.prehax.gomovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_logIn;
+    private TextView tv_signUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_logIn = (Button) findViewById(R.id.btn_logIn);
+        btn_logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tv_signUp = findViewById(R.id.tv_signUp);
+        tv_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Now let's signUp!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
