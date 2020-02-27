b02e5ca2af8f268609d71270d4f6dee13f3e1797
cheng <cheng530892953@gmail.com>
2020-02-27 18:51:37 -0500

    data storage1

--- app/src/main/java/com/prehax/gomovie/SignupActivity.java
+++ app/src/main/java/com/prehax/gomovie/SignupActivity.java
@@ -16,7 +16,8 @@ public class SignupActivity extends AppCompatActivity {
     private TextView tv_show,tv_show1,tv_show2;
     private Button btn_register;
     private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor,Email,password1,password2;
+    private SharedPreferences.Editor mEditor;
+    private String Email,password1,password2;
 
 
     @Override
@@ -39,30 +40,27 @@ public class SignupActivity extends AppCompatActivity {
         btn_register = findViewById(R.id.btn_register);
         mSharedPreferences = this.getSharedPreferences("PersonalInformation",MODE_PRIVATE);
         mEditor = mSharedPreferences.edit();
-        Email = mSharedPreferences.edit();
-        password1 = mSharedPreferences.edit();
-        password2 = mSharedPreferences.edit();
+
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // A temporary function for the button, I'm learning how to connect it to database
-                Email.putString("Email",et_rem.getText().toString());
-                password1.putString("Password1",et_rpass1.toString());
-                password2.putString("Password2",et_rpass2.toString());
+                password1 = et_rpass1.getText().toString().trim();
+                password2 = et_rpass2.getText().toString().trim();
+                Email = et_rem.getText().toString().trim();
                 if(password1.equals("")||(password2.equals(""))){//check it is empty or not
                     Toast.makeText(SignupActivity.this, "password is empty", Toast.LENGTH_SHORT).show();
                 }else if(password1.equals(password2)){
                     Toast.makeText(SignupActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                    mEditor.putString("Email",Email);
+                    mEditor.putString("password",password1);
                     mEditor.apply();
-                    Email.apply();
-                    password1.apply();
-                }else if(password1.equals("")!=password2.equals("")){
+                }else {
                     Toast.makeText(SignupActivity.this, "password not match", Toast.LENGTH_SHORT).show();
                 }
 //                Toast.makeText(SignupActivity.this, "Next step", Toast.LENGTH_SHORT).show();
                 tv_show.setText(mSharedPreferences.getString("Email",""));
-                tv_show1.setText(mSharedPreferences.getString("password1",""));
-                tv_show2.setText(mSharedPreferences.getString("password2",""));
+                tv_show1.setText(mSharedPreferences.getString("password",""));
             }
         });
 
