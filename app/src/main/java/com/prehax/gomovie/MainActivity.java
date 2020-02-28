6513d9feecb448429e94689170bb3a75510256f8
cheng <cheng530892953@gmail.com>
2020-02-27 22:05:40 -0500

    先这样吧，先睡了

--- app/src/main/java/com/prehax/gomovie/MainActivity.java
+++ app/src/main/java/com/prehax/gomovie/MainActivity.java
@@ -9,10 +9,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_logIn;
     private TextView tv_signUp, tv_reset;
+    private ArrayList<MovieGoer> list = new ArrayList<MovieGoer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
         btn_logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Login successfully!", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Login successfully!", Toast.LENGTH_SHORT).show();
+                read();
+                Toast.makeText(MainActivity.this, read(), Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent (MainActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
@@ -46,5 +54,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private String read(){
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = openFileInput("test.txt");
+            byte[] buff = new byte[1024];
+            StringBuilder sb = new StringBuilder("");
+            int len = 0;
+            while((len = fileInputStream.read(buff))>0){
+                sb.append(new String(buff,0,len));
+            }
+            return sb.toString();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(null != fileInputStream){
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
     }
 }
