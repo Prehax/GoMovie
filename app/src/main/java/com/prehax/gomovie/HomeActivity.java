dd060af322610412a4c4746900f727f563a424b8
Prehax <prehax@outlook.com>
2020-05-01 23:57:53 -0400

    Long0501 Fix Stuck at First

--- app/src/main/java/com/prehax/gomovie/HomeActivity.java
+++ app/src/main/java/com/prehax/gomovie/HomeActivity.java
@@ -152,7 +152,7 @@ public class HomeActivity extends AppCompatActivity {
                     startActivity(intent2);
                     return true;
                 case R.id.Showcard:
-                    Intent intent3 = new Intent(HomeActivity.this, WatchListActivity.class);
+                    Intent intent3 = new Intent(HomeActivity.this, CardActivity.class);
                     startActivity(intent3);
                     return true;
                 case R.id.WatchList:
@@ -209,6 +209,8 @@ public class HomeActivity extends AppCompatActivity {
                     });
                 case R.id.logout:
                     mAuth.signOut();
+                    Intent intent6 = new Intent(HomeActivity.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent6);
                     finish();
                 default:
                     return super.onOptionsItemSelected(item);
